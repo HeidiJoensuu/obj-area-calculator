086db234de35fa6f298a775256e050a2d8ed34cf
Heidi <cedi@hotmail.fi>
2021-03-24 23:54:51 +0200

    implement calculation

--- area.js
+++ area.js
@@ -12,24 +12,36 @@ fs.readFile(file, 'utf8', (err, data) => {
   data = data.split('# 1440 vertices');
   const rawDataVertices = data[0].split('\n')
   const rawDataTriangles = data[1].split('\n')
-  let triangles = []
-  let vertices = []
+  let total= 0
 
-  rawDataVertices.forEach(vertice => {
-    if (vertice.includes('v')) {
+  const vertices = rawDataVertices
+  .filter( vertice => vertice.includes('v'))
+    .map(vertice => {
       vertice = vertice.replace('v  ', '')
       vertice = vertice.replace('\r', '')
       const index = vertice.split(' ')
-      vertices.push({ x: index[0], y: index[1], z: index[2]})
-    }
+      return {x: Number(index[0]), y: Number(index[1]), z: Number(index[2])}
   });
 
-  rawDataTriangles.forEach(triangle => {
-    if (triangle.includes('f')) {
+  const triangles = rawDataTriangles
+    .filter( triangle => triangle.includes('f'))
+    .map(triangle => {
       triangle = triangle.replace('f ', '')
       triangle = triangle.replace('\r', '')
-    triangles.push(triangle)
-    }
+      const index = triangle.split(' ')
+      return { a: Number(index[0]-1), b: Number(index[1]-1), c: Number(index[2]-1)}
   });
 
+  triangles.forEach(triangle => {
+    const a1 = new Vector3(vertices[triangle.a].x, vertices[triangle.a].y, vertices[triangle.a].z)
+    const a2 = new Vector3(vertices[triangle.a].x, vertices[triangle.a].y, vertices[triangle.a].z)
+    const b = new Vector3(vertices[triangle.b].x, vertices[triangle.b].y, vertices[triangle.b].z)
+    const c = new Vector3(vertices[triangle.c].x, vertices[triangle.c].y, vertices[triangle.c].z)
+    const x = new Vector3(a1.substract(b))
+    const y = new Vector3(a2.substract(c))
+    const f = x.cross(y)
+    const length = f.length()/2
+    total+=length
+  })
+  console.log(Number.parseFloat(total).toFixed(3));
 })
