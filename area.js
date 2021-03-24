09f417bba3848bee21a39b339a0b140ee318948d
Heidi <cedi@hotmail.fi>
2021-03-24 20:19:13 +0200

    parsing .obj to arrays

--- area.js
+++ area.js
@@ -8,5 +8,28 @@ fs.readFile(file, 'utf8', (err, data) => {
     console.log(err);
     return
   }
-  console.log(data);
+
+  data = data.split('# 1440 vertices');
+  const rawDataVertices = data[0].split('\n')
+  const rawDataTriangles = data[1].split('\n')
+  let triangles = []
+  let vertices = []
+
+  rawDataVertices.forEach(vertice => {
+    if (vertice.includes('v')) {
+      vertice = vertice.replace('v  ', '')
+      vertice = vertice.replace('\r', '')
+      const index = vertice.split(' ')
+      vertices.push({ x: index[0], y: index[1], z: index[2]})
+    }
+  });
+
+  rawDataTriangles.forEach(triangle => {
+    if (triangle.includes('f')) {
+      triangle = triangle.replace('f ', '')
+      triangle = triangle.replace('\r', '')
+    triangles.push(triangle)
+    }
+  });
+
 })
